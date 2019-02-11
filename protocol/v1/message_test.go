0b9bb1abe3ccd42f44579037b4e171297981c2e7
Adam Babik <a.babik@designfortress.com>
2019-02-11 14:20:53 +0100

    fix TestEncodeMessage

--- protocol/v1/message_test.go
+++ protocol/v1/message_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 var (
-	testMessageBytes  = []byte(`["~#c4",["2","text/plain","~:public-group-user-message",154593077368201,1545930773682,["^ ","~:chat-id","testing-adamb","~:text","2"]]]`)
+	testMessageBytes  = []byte(`["~#c4",["abc123","text/plain","~:public-group-user-message",154593077368201,1545930773682,["^ ","~:chat-id","testing-adamb","~:text","abc123"]]]`)
 	testMessageStruct = StatusMessage{
-		Text:      "2",
+		Text:      "abc123",
 		ContentT:  "text/plain",
 		MessageT:  "public-group-user-message",
 		Clock:     154593077368201,
 		Timestamp: 1545930773682,
-		Content:   StatusMessageContent{"testing-adamb", "2"},
+		Content:   StatusMessageContent{"testing-adamb", "abc123"},
 	}
 )
 
@@ -27,5 +27,9 @@ func TestDecodeMessage(t *testing.T) {
 func TestEncodeMessage(t *testing.T) {
 	data, err := EncodeMessage(testMessageStruct)
 	require.NoError(t, err)
-	require.Equal(t, testMessageBytes, data)
+	// Decode it back to a struct because, for example, map encoding is non-deterministic
+	// and it is not possible to compare bytes.
+	val, err := DecodeMessage(data)
+	require.NoError(t, err)
+	require.EqualValues(t, testMessageStruct, val)
 }
