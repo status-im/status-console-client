c10ced176a1a9394afd5617d9a92ce12fcfbc56b
decanus <dean.eigenmann@icloud.com>
2019-05-11 22:58:28 +0200

    started some work

---
+++ protocol/adapters/datasync_client.go
@@ -0,0 +1,51 @@
+package adapters
+
+import (
+	"context"
+	"encoding/hex"
+
+	"github.com/status-im/mvds"
+	"github.com/status-im/status-console-client/protocol/v1"
+	"github.com/status-im/status-go/node"
+	whisper "github.com/status-im/whisper/whisperv6"
+)
+
+// DataSyncClient is an adapter for MVDS
+// that implements the Protocol interface.
+type DataSyncClient struct {
+	sync mvds.Node
+
+	node *node.StatusNode // TODO: replace with an interface
+	shh  *whisper.Whisper
+}
+
+func (*DataSyncClient) Subscribe(ctx context.Context, messages chan<- *protocol.Message, options protocol.SubscribeOptions) (*protocol.Subscription, error) {
+	panic("implement me")
+}
+
+func (c *DataSyncClient) Send(ctx context.Context, data []byte, options protocol.SendOptions) ([]byte, error) {
+
+	if err := options.Validate(); err != nil {
+		return nil, err
+	}
+
+	newMessage, err := newNewMessage(a.keysManager, data) // @todo
+	if err != nil {
+		return nil, err
+	}
+	if err := updateNewMessageFromSendOptions(newMessage, options); err != nil {
+		return nil, err
+	}
+
+	err := c.sync.AppendMessage(newMessage.Topic, newMessage.MarshalJSON())
+	if err != nil {
+		return nil, err
+	}
+
+	return hex.DecodeString(hash)
+
+}
+
+func (*DataSyncClient) Request(ctx context.Context, params protocol.RequestOptions) error {
+	panic("implement me")
+}
