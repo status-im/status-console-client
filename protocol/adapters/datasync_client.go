7806d8820c84d835092688449b6041b2144b018e
decanus <dean.eigenmann@icloud.com>
2019-05-14 00:57:02 +0200

    answered lol

--- protocol/adapters/datasync_client.go
+++ protocol/adapters/datasync_client.go
@@ -30,6 +30,7 @@ func (c *DataSyncClient) Send(ctx context.Context, data []byte, options protocol
 	}
 
 	// @todo I think we need to wrap data into a protocol.Message?
+	// @todo yes I do.
 
 	topic, err := topic(options)
 	if err != nil {
