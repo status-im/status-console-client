82ebeecc81034ee8f198750abcdc5b7736d2b15c
decanus <dean.eigenmann@icloud.com>
2019-06-10 09:28:10 -0700

    minor updates

--- protocol/datasync/syncstate.go
+++ protocol/datasync/syncstate.go
@@ -10,19 +10,40 @@ type SyncState struct {
 	db *sql.DB
 }
 
-func (s *SyncState) Get(group mvds.GroupID, id mvds.MessageID, sender mvds.PeerID) interface{} {
+func (s *SyncState) Get(group mvds.GroupID, id mvds.MessageID, peer mvds.PeerID) mvds.State {
 	panic("implement me")
 }
 
-func (s *SyncState) Set(group mvds.GroupID, id mvds.MessageID, sender mvds.PeerID, newState interface{}) {
+func (s *SyncState) Set(group mvds.GroupID, id mvds.MessageID, peer mvds.PeerID, newState mvds.State) {
 	panic("implement me")
 }
 
-func (s *SyncState) Remove(group mvds.GroupID, id mvds.MessageID, sender mvds.PeerID) {
-	s.db.Query("")
+func (s *SyncState) Remove(group mvds.GroupID, id mvds.MessageID, peer mvds.PeerID) {
+	q, err := s.db.Prepare("DELETE state WHERE groupID = ? AND id = ? AND peer = ?")
+	if err != nil {
+		// @todo
+		return
+	}
+
+	r, err := q.Exec(group[:], id[:], peer[:])
+	if err != nil {
+		// @todo
+		return
+	}
+
+	// @todo check r.RowsAffected
+	affected, err := r.RowsAffected()
+	if err != nil {
+		// @todo
+		return
+	}
+
+	if affected != 1 {
+		// @todo
+	}
 }
 
-func (s *SyncState) Map(process func(mvds.GroupID, mvds.MessageID, mvds.PeerID, interface{}) interface{}) {
+func (s *SyncState) Map(process func(mvds.GroupID, mvds.MessageID, mvds.PeerID, mvds.State) mvds.State) {
 	panic("implement me")
 }
 
